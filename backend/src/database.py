6bbeff29a160ffe1cb3faaa0d8cd99125f314b2d
pellegrinot <tommaso.pellegrino@linksmt.it>
2026-01-17 23:02:21 +0100

    work on pin

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,7 +1,5 @@
 import sqlite3
 from pathlib import Path
-from typing import Optional
-
 
 def get_connection(db_path: Path) -> sqlite3.Connection:
     """Create a new database connection with foreign keys enabled."""
@@ -9,7 +7,6 @@ def get_connection(db_path: Path) -> sqlite3.Connection:
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA foreign_keys = ON")
     return conn
-
 
 def init_db(db_path: Path, schema_path: Path):
     """Initialize the database by executing the schema."""
@@ -22,7 +19,6 @@ def init_db(db_path: Path, schema_path: Path):
         conn.commit()
     finally:
         conn.close()
-
 
 def seed_dummy_data(db_path: Path):
     """Fill the database with dummy olympiads, players, and events."""
@@ -63,74 +59,5 @@ def seed_dummy_data(db_path: Path):
         )
 
         conn.commit()
-    finally:
-        conn.close()
-
-
-# def get_olympiads(db_path: Path) -> list[dict]:
-#     conn = get_connection(db_path)
-#     try:
-#         cursor = conn.execute(f"SELECT id, name, version FROM olympiads")
-#         return [{"id": row["id"], "name": row["name"], "version": row["version"]} for row in cursor.fetchall()]
-#     finally:
-#         conn.close()
-
-# def get_entities(db_path: Path, entity_type: str, olympiad_id: str) -> Optional[list[dict]]:
-#     """Retrieve all entities of a given type (olympiads, players, events)."""
-#     conn = get_connection(db_path)
-#     try:
-#         if olympiad_id:
-#             cursor = conn.execute(f"SELECT id FROM olympiads WHERE id = {olympiad_id}")
-#             row = cursor.fetchone()
-#             if row:
-#                 cursor = conn.execute(
-#                     f"SELECT e.id, e.name, e.version FROM {entity_type} e JOIN olympiads o ON o.id = e.olympiad_id WHERE o.id = {olympiad_id}"
-#                 )
-#                 return [{"id": row["id"], "name": row["name"], "version": row["version"]} for row in cursor.fetchall()]
-#             else:
-#                 # olympaid_id does not exist anymore in the olympiads database
-#                 return None
-#         else:
-#             # User might not have selected any olympiad (empty olympiad badge)
-#             return None
-#     finally:
-#         conn.close()
-
-
-def create_entity(db_path: Path, entity_type: str, name: str) -> dict:
-    """Create a new entity and return it with id, name, and version."""
-    allowed_tables = {"olympiads", "players", "events"}
-    if entity_type not in allowed_tables:
-        raise ValueError(f"Invalid entity type: {entity_type}")
-
-    conn = get_connection(db_path)
-    try:
-        if entity_type == "olympiads":
-            cursor = conn.execute(
-                "INSERT INTO olympiads (name, pin) VALUES (?, ?) RETURNING id, name, version",
-                (name, "0000")
-            )
-        else:
-            raise ValueError(f"Creating {entity_type} without olympiad_id not supported yet")
-
-        row = cursor.fetchone()
-        conn.commit()
-        return {"id": row["id"], "name": row["name"], "version": row["version"]}
-    finally:
-        conn.close()
-
-
-def get_olympiad(db_path: Path, olympiad_id: int) -> dict | None:
-    """Retrieve a single olympiad by ID."""
-    conn = get_connection(db_path)
-    try:
-        cursor = conn.execute(
-            "SELECT id, name, version FROM olympiads WHERE id = ?",
-            (olympiad_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
-            return None
-        return {"id": row["id"], "name": row["name"], "version": row["version"]}
     finally:
         conn.close()
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,8 @@
 import os
+import random
+import sqlite3
 import uvicorn
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, Depends
 from fastapi.responses import JSONResponse, HTMLResponse, Response
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
@@ -14,7 +16,25 @@ schema_path = Path(os.environ["SCHEMA_PATH"])
 root = Path(os.environ["PROJECT_ROOT"])
 templates = Jinja2Templates(directory=root / "frontend" / "templates")
 
-entity_labels = { "olympiads": "olimpiadi", "players": "giocatori", "events": "eventi" }
+entity_list_form_placeholder = {
+    "olympiads": "Aggiungi una nuova olimpiade",
+    "players": "Aggiungi un nuovo giocatore",
+    "events": "Aggiungi un nuovo evento",
+    "teams": "Aggiungi un nuovo team",
+}
+
+select_olympiad_message = {
+    "players": "Seleziona un'olimpiade per visualizzare tutti i giocatori",
+    "events": "Seleziona un'olimpiade per visualizzare tutti gli eventi",
+    "teams": "Seleziona un'olimpiade per visualizzare tutti i team",
+}
+
+def get_db():
+    conn = database.get_connection(db_path)
+    try:
+        yield conn
+    finally:
+        conn.close()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -43,61 +63,107 @@ async def serve_css(filename: str):
     css_path = root / "frontend" / f"{filename}.css"
     return Response(content=css_path.read_text(), media_type="text/css")
 
-@app.post("/api/{entities}")
-async def create_entity(request: Request, entities: str, name: str = Form(...), olympiad_id: str = Form(default="")):
-    conn = database.get_connection(db_path)
-    try:
-        if entities == "olympiads":
-            try:
-                # query that add an olympiad with name
-                cursor = conn.execute(f"INSERT INTO olympiads (name, pin) VALUES ({name}, {9999})")
-            except:
-                # handle case in which name already exist
-                # I want to return the html of a modal window with a message that says "olympiad name already used"
-                # and a button with "refresh page" that when is pressed will refresh the olympiads page
-                # which means executing the query that retrieve the list of (olympiad_id, name, version) and render the entity_list.html template
-                # which basically means reloading the olympiads page almost from scratch except for the olympiad badge
-        else:
-            raise NotImplementedError
-    finally:
-        conn.close()
+@app.post("/api/olympiads")
+async def create_olympiad(request: Request, name: str = Form(...)):
+    response = templates.TemplateResponse(
+        request, "create_olympiad_pin.html", {"name": name}
+    )
+    response.headers["HX-Retarget"] = "#modal-container"
+    return response
 
-@app.get("/api/{entities}")
-async def get_entities(request: Request, entities: str, olympiad_id: str = ""):
-    conn = database.get_connection(db_path)
+@app.post("/api/olympiads/confirm_creation")
+async def create_olympiad_with_pin(
+    request: Request, name: str = Form(...), pin: str = Form(...), conn = Depends(get_db)
+):
+    if len(pin) != 4:
+        response = templates.TemplateResponse(
+            request, "create_olympiad_pin.html",
+            {"name": name, "error": "Il PIN deve essere composto da 4 cifre"}
+        )
+        response.headers["HX-Retarget"] = "#modal-container"
+        return response
     try:
-        if entities == "olympiads":
-            cursor = conn.execute(f"SELECT id, name, version FROM olympiads")
-            rows = [{"id": row["id"], "name": row["name"], "version": row["version"]} for row in cursor.fetchall()]
-        else:
-            if olympiad_id:
-                cursor = conn.execute(f"SELECT id FROM olympiads WHERE id = {olympiad_id}")
-                row = cursor.fetchone()
-                if row:
-                    cursor = conn.execute(
-                        f"SELECT e.id, e.name, e.version FROM {entities} e JOIN olympiads o ON o.id = e.olympiad_id WHERE o.id = {olympiad_id}"
-                    )
-                    rows = [{"id": row["id"], "name": row["name"], "version": row["version"]} for row in cursor.fetchall()]
-                else:
-                    return templates.TemplateResponse(request, "olympiad_not_found.html")
-            else:
-                return templates.TemplateResponse(request, "select_olympiad_required.html", {"entity_label": entity_labels[entities]})
-    finally:
-        conn.close()
-
+        conn.execute("INSERT INTO olympiads (name, pin) VALUES (?, ?)", (name, pin))
+    except sqlite3.IntegrityError:
+        return templates.TemplateResponse(
+            request, "duplicate_name_error.html",
+            {"message": "Una olimpiade con questo nome è già presente", "entities": "olympiads"}
+        )
+    conn.commit()
+    cursor = conn.execute("SELECT id, name, version FROM olympiads")
+    rows = [{"id": row["id"], "name": row["name"], "version": row["version"]} for row in cursor.fetchall()]
+    placeholder = entity_list_form_placeholder["olympiads"]
     return templates.TemplateResponse(
-        request,
-        "entity_list.html",
-        {"entities": entities, "entity_label": entity_labels[entities], "items": rows}
+        request, "entity_list.html",
+        {"entities": "olympiads", "placeholder": placeholder, "items": rows}
     )
 
+@app.post("/api/players")
+async def create_player(request: Request, name: str = Form(...), olympiad_id: str = Form(...), conn = Depends(get_db)):
+    cursor = conn.execute(f"SELECT id, name, version FROM olympiads WHERE id = ?", (olympiad_id))
+    row = cursor.fetchone()
+    if not row:
+        return templates.TemplateResponse(request, "olympiad_not_found.html")
+
+    try:
+        conn.execute(f"INSERT INTO players (olympiad_id, name) VALUES (?, ?)", (olympiad_id, name))
+    except sqlite3.IntegrityError:
+        return templates.TemplateResponse(
+            request, "duplicate_name_error.html", {"message": "Un giocatore con questo nome è già presente", "entities": "players"}
+        )
+    conn.commit()
+    cursor = conn.execute(
+        f"SELECT p.id, p.name, p.version FROM players p JOIN olympiads o ON o.id = p.olympiad_id WHERE o.id = ?",
+        (olympiad_id,)
+    )
+    rows = [{"id": row["id"], "name": row["name"], "version": row["version"]} for row in cursor.fetchall()]
+    placeholder = entity_list_form_placeholder["players"]
+    return templates.TemplateResponse(
+        request, "entity_list.html", {"entities": "players", "placeholder": placeholder, "items": rows}
+    )
+
+@app.get("/api/olympiads")
+async def get_olympiads(request: Request, conn = Depends(get_db)):
+    cursor = conn.execute(f"SELECT id, name, version FROM olympiads")
+    rows = [{"id": row["id"], "name": row["name"], "version": row["version"]} for row in cursor.fetchall()]
+    placeholder = entity_list_form_placeholder["players"]
+    return templates.TemplateResponse(
+        request, "entity_list.html", {"entities": "olympiads", "placeholder": placeholder, "items": rows}
+    )
+
+# TODO: fix security issues
+@app.get("/api/{entities}")
+async def get_entities(request: Request, entities: str, olympiad_id: str = "", conn = Depends(get_db)):
+    if olympiad_id:
+        cursor = conn.execute(f"SELECT id FROM olympiads WHERE id = {olympiad_id}")
+        row = cursor.fetchone()
+        if row:
+            cursor = conn.execute(
+                f"SELECT e.id, e.name, e.version FROM {entities} e JOIN olympiads o ON o.id = e.olympiad_id WHERE o.id = ?",
+                (olympiad_id,)
+            )
+            rows = [{"id": row["id"], "name": row["name"], "version": row["version"]} for row in cursor.fetchall()]
+            placeholder = entity_list_form_placeholder[entities]
+            return templates.TemplateResponse(
+                request, "entity_list.html", {"entities": entities, "placeholder": placeholder, "items": rows}
+            )
+        else:
+            return templates.TemplateResponse(request, "olympiad_not_found.html")
+    else:
+        return templates.TemplateResponse(request, "select_olympiad_required.html", {"message": select_olympiad_message[entities]})
+
 @app.get("/api/olympiads/{olympiad_id}")
-async def select_olympiad(request: Request, olympiad_id: int):
+async def select_olympiad(request: Request, olympiad_id: int, conn = Depends(get_db)):
     """Select an olympiad and update the olympiad badge"""
-    db_path = Path(os.environ["DATABASE_PATH"])
-    olympiad = database.get_olympiad(db_path, olympiad_id)
-    if olympiad is None:
-        return HTMLResponse(content="Olympiad not found", status_code=404)
+
+    cursor = conn.execute("SELECT id, name, version FROM olympiads WHERE id = ?", (olympiad_id,))
+    row = cursor.fetchone()
+    if not row:
+        response = templates.TemplateResponse(request, "olympiad_not_found.html")
+        response.headers["HX-Retarget"] = "#main-content"
+        return response
+
+    olympiad = {"id": row["id"], "name": row["name"], "version": row["version"]}
     return templates.TemplateResponse(request, "olympiad_badge.html", {"olympiad": olympiad})
 
 
